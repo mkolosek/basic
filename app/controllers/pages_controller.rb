e82e18c94adb3d8423f85841425a3c44ed2d7389
mkolosek <marko.milosevic@kolosek.com>
2018-11-28 16:25:52 +0100

    Testing linters

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def welcome
-    
+    puts  'this should not be here'
+       puts 'This is a very long line. This is a very long line. This is a very long line. This is a very long line. This is a very long line. This is a very long line. This is a very long line. This is a very long line. '
   end
 
 end
