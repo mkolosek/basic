9187b52716711424c8e97e13eb67300d78d41e25
mkolosek <marko.milosevic@kolosek.com>
2018-11-29 10:25:02 +0100

    Additional linter error

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,10 @@ class PagesController < ApplicationController
     puts  'this should not be here'
        puts 'This is a very long line. This is a very long line. This is a very long line. This is a very long line. This is a very long line. This is a very long line. This is a very long line. This is a very long line. '
     puts   'Inconsistent spacing.....'
+
+    puts 'This is a very long line. This is a very long line. This is a very long line. This is a very long line. This is a very long line. This is a very long line. This is a very long line. This is a very long line. '
+
+
   end
 
 end
